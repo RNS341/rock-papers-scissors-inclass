3aad97fc9b47ad4b4d9112b85ee78737fa425b46
Rob Sansone <50809985+RNS341@users.noreply.github.com>
2019-06-09 16:25:38 -0400

    example test

--- game.py
+++ game.py
@@ -1,20 +1,68 @@
 # game.py
 
-print("Rock, Paper, Scissors, Shoot!") 
+import random
 
-# Capture Inputs
+print("Rock, Paper, Scissors, Shoot!") # this is also a comment
+
+# CAPTURE INPUTS
 
 user_choice = input("Please choose one of the following options: 'rock', 'paper', or 'scissors' (without the quotes):")
-print("-------------")
-print("User Choice: ",user_choice)
 
-# Validate Inputs
+print("--------------")
+print("USER CHOICE:", user_choice)
 
-if user_choice not in ["rock","paper","scissors"]:   
-    print("invalid selection please try again")
+# VALIDATE INPUTS
+
+options = ["rock", "paper", "scissors"]
+
+if user_choice not in options:
+    print("INVALID SELECTION, PLEASE TRY AGAIN...")
     exit()
 
-# Generate Computer Selection
-print("Generating...")
+# GENERATE COMPUTER SELECTION
 
-# Display Final Outputs / Outcomes
+computer_choice = random.choice(options)
+
+print("--------------")
+print("GENERATING...")
+print("COMPUTER CHOICE:", computer_choice)
+
+# DETERMINE THE WINNER
+#
+# rock beats scissors
+# paper beats rock
+# scissors beats paper
+# same selections is a tie
+#
+# first attribute represents the user, second represents the computer
+winners = {
+    "rock":{
+        "rock": None,
+        "paper": "paper",
+        "scissors": "rock",
+    },
+    "paper":{
+        "rock": "paper",
+        "paper": None,
+        "scissors": "scissors",
+    },
+    "scissors":{
+        "rock": "rock",
+        "paper": "scissors",
+        "scissors": None,
+    },
+}
+
+winning_choice = winners[user_choice][computer_choice]
+
+# DISPLAY FINAL OUTPUTS / OUTCOMES
+
+if winning_choice:
+    if winning_choice == user_choice:
+        print("YOU WON")
+    elif winning_choice == computer_choice:
+        print("YOU LOST")
+else:
+    print("TIE")
+
+print("Thanks for playing. Please play again!")
